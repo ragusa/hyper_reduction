01963b2b72233831fe56622f67ff1328f584bb27
ragusa <jean.ragusa@tamu.edu>
2023-09-19 21:42:16 -0500

    obj writer

--- triangle_mesh_export_wavefront.py
+++ triangle_mesh_export_wavefront.py
@@ -6,7 +6,7 @@ Created on Wed Sep 13 23:12:09 2023
 @author: jean.ragusa
 """
 
-#%% import python module
+# %% import python module
 from __future__ import absolute_import
 from __future__ import print_function
 
@@ -21,7 +21,9 @@ import meshpy.triangle as triangle
 
 plt.close("all")
 
-#%% small utility function to create facets given a list of points
+# %% small utility function to create facets given a list of points
+
+
 def round_trip_connect(start, end):
     result = []
     for i in range(start, end):
@@ -30,10 +32,10 @@ def round_trip_connect(start, end):
     return result
 
 
-#%% definition of the 2D geometry using planar straight line segments 
+# %% definition of the 2D geometry using planar straight line segments
 # to be then meshed with the Triangle mesh generator, via MeshPy
 
-max_vol = 1e-3
+max_vol = 1e-2
 
 #              (2)
 #      5------------------4
@@ -101,45 +103,45 @@ mesh = triangle.build(
 print(("%d elements" % len(mesh.elements)))
 print(("%d vertices" % len(mesh.points)))
 
-#%% extract meshing data into standard numpy ndarrays
+# %% extract meshing data into standard numpy ndarrays
 
 # el2pt = np.array(mesh.elements, dtype=int)
 print(mesh.elements[0])
-el2pt = np.zeros((len(mesh.elements),3),dtype=int)
+el2pt = np.zeros((len(mesh.elements), 3), dtype=int)
 for i in range(len(mesh.elements)):
-    el2pt[i,:] = mesh.elements[i]
-    
+    el2pt[i, :] = mesh.elements[i]
+
 # el2at = np.array(mesh.element_attributes, dtype=int)
 print(mesh.element_attributes[0])
-el2at = np.zeros(len(mesh.element_attributes),dtype=int)
+el2at = np.zeros(len(mesh.element_attributes), dtype=int)
 for i in range(len(mesh.element_attributes)):
     el2at[i] = mesh.element_attributes[i]
 
 # el2ne = np.array(mesh.neighbors, dtype=int)
 print(mesh.neighbors[0])
-el2ne = np.zeros((len(mesh.neighbors),3),dtype=int)
+el2ne = np.zeros((len(mesh.neighbors), 3), dtype=int)
 for i in range(len(mesh.neighbors)):
-    el2ne[i,:] = mesh.neighbors[i]
+    el2ne[i, :] = mesh.neighbors[i]
 
 # fa2pt = np.array(mesh.faces, dtype=int)
 print(mesh.faces[0])
-fa2pt = np.zeros((len(mesh.faces),2),dtype=int)
+fa2pt = np.zeros((len(mesh.faces), 2), dtype=int)
 for i in range(len(mesh.faces)):
-    fa2pt[i,:] = mesh.faces[i]
+    fa2pt[i, :] = mesh.faces[i]
 
 # fa2ma = np.array(mesh.face_markers, dtype=int)
 print(mesh.face_markers[0])
-fa2ma = np.zeros(len(mesh.face_markers),dtype=int)
+fa2ma = np.zeros(len(mesh.face_markers), dtype=int)
 for i in range(len(mesh.face_markers)):
     fa2ma[i] = mesh.face_markers[i]
 
 # pt2xy = np.array(mesh.points)
 print(mesh.points[0])
-pt2xy = np.zeros((len(mesh.points),2))
+pt2xy = np.zeros((len(mesh.points), 2))
 for i in range(len(mesh.points)):
-    pt2xy[i,:] = mesh.points[i]
+    pt2xy[i, :] = mesh.points[i]
 
-#%% plot mesh if requested
+# %% plot mesh if requested
 do_plot = True
 axis_range = [-0.15, 1.15, -0.15, 1.15]
 
@@ -176,17 +178,78 @@ if do_plot:
             )
     plt.show()
 
-#%% save meshing data in txt files, if requested
+# %% save meshing data in txt files, if requested
 save_meshing_data_to_txt = False
 
 if save_meshing_data_to_txt:
-    basename = 'Triangular_mesh_nelems' + str(len(el2pt[:,0]))
-    np.savetxt(basename+'_el2pt.txt',el2pt, fmt="%i")
-    np.savetxt(basename+'_el2at.txt',el2at, fmt="%i")
-    np.savetxt(basename+'_fa2pt.txt',fa2pt, fmt="%i")
-    np.savetxt(basename+'_fa2ma.txt',fa2ma, fmt="%i")
-    np.savetxt(basename+'_pt2xy.txt',pt2xy)
-    np.savetxt(basename+'_el2ne.txt',el2ne, fmt="%i")
+    basename = 'Triangular_mesh_nelems' + str(len(el2pt[:, 0]))
+    np.savetxt(basename+'_el2pt.txt', el2pt, fmt="%i")
+    np.savetxt(basename+'_el2at.txt', el2at, fmt="%i")
+    np.savetxt(basename+'_fa2pt.txt', fa2pt, fmt="%i")
+    np.savetxt(basename+'_fa2ma.txt', fa2ma, fmt="%i")
+    np.savetxt(basename+'_pt2xy.txt', pt2xy)
+    np.savetxt(basename+'_el2ne.txt', el2ne, fmt="%i")
 
-#%% convert to wavefront obj
+# %% convert to wavefront obj
+dest_file = 'tri_2mat_bc.obj'
 
+with open(dest_file, 'w') as f:
+    f.write('# triangular mesh generated with Triangle thru MeshPy\n')
+    f.write('# number of triangles: {}\n'.format(el2pt.shape[0]))
+    f.write('# number of material attributes: {}\n'.format(
+        len(np.unique(el2at))))
+    f.write('# number of boundary markers: {}\n'.format(
+        len(np.unique(fa2ma))))
+    f.write('mtllib untitled.mtl \n')
+    f.write('o main_object \n')
+
+    # write mesh vertices
+    for pt in pt2xy:
+        f.write('v {} {} {}\n'.format(pt[0], pt[1], 0.))
+
+    # write normal
+    f.write('vn -0.0000 -0.0000 1.0000 \n')
+    # write shading
+    f.write('s 1 \n')
+
+    # write triangles per material attribute
+    for m in np.unique(el2at):
+        f.write('g mat{} \n'.format(m))
+        f.write('usemtl Mat{} \n'.format(m))
+        for att, tri in zip(el2at, el2pt):
+            if att == m:
+                # converting to 1-indexing
+                f.write(
+                    'f {}//1 {}//1 {}//1 \n'.format(tri[0]+1, tri[1]+1, tri[2]+1))
+
+    # find external edges (face marker>0)
+    ind = np.where(fa2ma != 0)[0]
+    fa2ma = fa2ma[ind]
+    fa2pt = fa2pt[ind, :]
+
+    # number of vertices in the main object
+    nvert = pt2xy.shape[0]
+
+    # write edges per bc attribute
+    for bdID in np.unique(fa2ma):
+        # get bd edge vertices
+        vert_list = []
+        f.write('o bd_object{} \n'.format(bdID))
+        for bdatt, face in zip(fa2ma, fa2pt):
+            if bdatt == bdID:
+                vert_list.append(face)
+        vertID = np.reshape(np.asarray(vert_list), (-1,))
+        # make vertex ID unique
+        vertID_uniq, inv_ind = np.unique(vertID, return_inverse=True)
+        # write vertex coordinates
+        for v in vertID_uniq:
+            pt = pt2xy[v, :]
+            f.write('v {} {} {}\n'.format(pt[0], pt[1], 0.))
+        # write lines
+        inv_ind = np.reshape(inv_ind, (-1, 2))
+        for edg in inv_ind:
+            # converting to 1-indexing
+            # adding nvert
+            f.write('l {} {} \n'.format(edg[0]+1+nvert, edg[1]+1+nvert))
+        # increment number of nvert used after each boundary object
+        nvert += len(vertID_uniq)
